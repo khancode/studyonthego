a05467fd723983c2e1d6ce6d8aa8b87e9d39e895
Omar Khan <khancodegt@gmail.com>
2015-03-16 00:16:21 -0400

    GetStudyGroups http get request refactored.

--- src/com/studyonthegoapp/restfulapi/GetStudyGroups.java
+++ src/com/studyonthegoapp/restfulapi/GetStudyGroups.java
@@ -2,15 +2,12 @@ package com.studyonthegoapp.restfulapi;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,9 +19,6 @@ import android.os.AsyncTask;
 
 public class GetStudyGroups extends AsyncTask<String, Void, Void>
 {	
-//	private ArrayList<HashMap<String, String>> messages;
-//	
-//	private MessagingFragment messagingActivity;
 	
 	private ArrayList<StudyGroup> studyGroups;
 	private SearchStudyGroupsFragment searchStudyGroupFragment;
@@ -37,138 +31,66 @@ public class GetStudyGroups extends AsyncTask<String, Void, Void>
 	@Override
 	protected Void doInBackground(String... params) {
 		
-//		String username = params[0];
-//		String friendUsername = params[1];
-//		String dateTime = params[2];
+//		String course = params[0];
+//		String building = params[1];
 		
-		// Create a new HttpClient and Post Header
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpGet httppost = new HttpGet("http://studyonthegoapp.com/rest/studygroups/show");
-        JSONObject json = new JSONObject();
-        
-        // Will hold the whole all the data gathered from the URL
-     	String result = null;
- 
-        try {
-//            // JSON data:
-//            json.put("username", username);
-//            json.put("friendUsername", friendUsername);
-//            json.put("dateTime", dateTime);
-// 
-//            JSONArray postjson = new JSONArray();
-//            postjson.put(json);
-//// 
-//            // Post the data:
-//            httppost.setHeader("json",json.toString());
-//            httppost.getParams().setParameter("jsonpost",postjson);
- 
-            // Execute HTTP Post Request
-            System.out.print(json);
-            HttpResponse response = httpclient.execute(httppost);
- 
-            // for JSON:
-            if(response != null)
-            {
-                InputStream is = response.getEntity().getContent();
- 
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-                StringBuilder sb = new StringBuilder();
- 
-                String line = null;
-                try {
-                    while ((line = reader.readLine()) != null) {
-                        sb.append(line + "\n");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        is.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                
-                result = sb.toString();
-            }
- 
- 
-        }catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-        }
-        
-        
-        // Holds Key Value pairs from a JSON source
-		JSONObject jsonObject;
+		String url = "http://www.studyonthegoapp.com/rest/studygroups/show";
+		
+		StringBuffer response = new StringBuffer();
+		
 		try {
-
-			// Print out all the data read in
-			// Log.v("JSONParser RESULT ", result);
-
-			// Get the root JSONObject
-//			jsonObject = new JSONObject(result);
-			
-//			if (result.equals("null"))
-				
-//			System.out.println(); // Consume line
-//			System.out.println(result);
-//			
-//			if (result.equals("none"))
-//			{
-//				System.out.println("dude");
-//				messages = null;
-//				return null;
-//			}
-			
-			JSONArray jArray = new JSONArray(result);
-			
-//			messages = new ArrayList<HashMap<String, String>>();
-			
+			URL obj = new URL(url);
+		
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	 
+			// optional default is GET
+			con.setRequestMethod("GET");
+	 
+			//add request header
+//			con.setRequestProperty("User-Agent", "USER_AGENT");
+	 
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'GET' request to URL : " + url);
+			System.out.println("Response Code : " + responseCode);
+	 
+			BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			response = new StringBuffer();
+	 
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+	 
+			//print result
+//			System.out.println(response.toString());
+		
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			JSONArray jArray = new JSONArray(response.toString());
+		
 			studyGroups = new ArrayList<StudyGroup>();
 			
 			for(int i = 0; i < jArray.length(); i++)
 			{
-			   jsonObject = jArray.getJSONObject(i);
-			   
+			   JSONObject jsonObject = jArray.getJSONObject(i);
+	
 			   studyGroups.add(new StudyGroup(jsonObject));
-			   
-//			   HashMap<String, String> messageRow = new HashMap<String, String>();
-//			   
-//			   messageRow.put("fromUsername", jsonObject.getString("FromUsername"));
-//			   messageRow.put("toUsername", jsonObject.getString("ToUsername"));
-//			   messageRow.put("originalMessage", jsonObject.getString("OriginalMessage"));
-//			   messageRow.put("modifiedMessage", jsonObject.getString("ModifiedMessage"));
-//			   messageRow.put("dateTime", jsonObject.getString("DateTime"));
-//			   messageRow.put("status", jsonObject.getString("Status"));
-//			   messageRow.put("fromMisses", jsonObject.getString("FromMisses"));
-//			   messageRow.put("fromTimeElapsed", jsonObject.getString("FromTimeElapsed"));
-//			   
-//			   messageRow.put("toMisses", jsonObject.getString("ToMisses"));
-//			   messageRow.put("toTimeElapsed", jsonObject.getString("ToTimeElapsed"));
-//			   messageRow.put("fromPoints", jsonObject.getString("FromPoints"));
-//			   messageRow.put("toPoints", jsonObject.getString("ToPoints"));
-//			   messageRow.put("winner", jsonObject.getString("Winner"));
-//			   
-//			   messages.add(messageRow);
 			}
-			
-			// Check if the MySQL query caused an error
-//			boolean mysqlError = jsonObject.optBoolean("mysqlError");
-//			
-//			if (mysqlError)
-//			{
-//				fromUsername = message = dateTime = "MySQL error";
-//				return null;
-//			}
-
-		} catch (JSONException e) {
-//			messages = null;
-			studyGroups = null;
-//			e.printStackTrace();
 		}
-
+		catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         return null;
 	}
